2ba82b836af806dfd3b46fd844c6cd47f285010d
aastha <patela13@students.rowan.edu>
2023-04-14 20:40:17 -0400

    Initial commit

---
+++ device/portmap.c
@@ -0,0 +1,12 @@
+#include "portmap.h"
+uint8_t inb(uint16_t port) {
+             uint8 ret;
+             __asm__ __volatile__("inb %1, %0" : "=a" (ret) : "Nd" (port));
+             return ret;
+}
+void outb(uint16_t port, uint_t val) {
+__asm__ __volatile__(“outb %0, %1” : : “a” (val), “Nd” (port));
+}
+
+           
+
---
+++ include/portmap.h
@@ -0,0 +1,3 @@
+uint8_t inb(uint16_t port);
+void outb(uint16_t port,uint8_t val);
+
